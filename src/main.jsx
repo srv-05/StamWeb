4d85f0024050579d1009ba1db18bbe248e6df6bd
Q-SRV <sumathrengasami@gmail.com>
2025-12-16 13:03:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { BlogProvider } from './context/BlogContext' // <--- IMPORT THIS
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <BlogProvider> {/* <--- WRAP EVERYTHING INSIDE THIS */}
+        <App />
+      </BlogProvider>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
